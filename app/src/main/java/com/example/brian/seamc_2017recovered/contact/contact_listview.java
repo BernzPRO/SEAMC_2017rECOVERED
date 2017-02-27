77e91390adabf98567ca2c2b6fe61296183e55aa
jessindo <jessindo@gmail.com>
2017-02-27 15:44:31 +0700

    contact list updated

--- app/src/main/java/com/example/brian/seamc_2017recovered/contact/contact_listview.java
+++ app/src/main/java/com/example/brian/seamc_2017recovered/contact/contact_listview.java
@@ -28,9 +28,10 @@ public class contact_listview extends Activity {
         mContactList = new ArrayList<>();
         //Add sample data for list
         //We can get data from DB, webservice here
-        mContactList.add(new contactlis(1, "Mrs Shah", "Whole", "0812 xxxx xxxx","simi_shah@bsj.sch.id"));
+        mContactList.add(new contactlis(1, "Mrs Shah", "Teacher In Charge", "0812 xxxx xxxx","simi_shah@bsj.sch.id"));
         mContactList.add(new contactlis(2, "Seong Bin Cho", "App Programmer", "0812 8694 8291","18chos@bsj.sch.id"));
-
+        mContactList.add(new contactlis(3, "Jessica Hadiwijoyo", "Co-programmer", "0812 1277 0624", "18hadiwijoyoj@bsj.sch.id"));
+        mContactList.add(new contactlis(4, "Atria Hotel", "Hotel", "(021) 29215999","-"));
 
         //Init adapter
         adapter = new contact_adapter(getApplicationContext(), mContactList);
